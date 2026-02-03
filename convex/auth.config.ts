ed8d5b8784cd85417476c056945fc99dfd5a4519
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2026-02-03 10:51:06 +0530

    Implementing Webhooks

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -9,7 +9,7 @@ export default {
       // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
       // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain: "https://talented-tadpole-19.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN || "https://talented-tadpole-19.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
